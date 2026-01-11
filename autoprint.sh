b52e3b7d4b60dce457cefa8fef8efe8aa8e4f904
MAC0xFF <amicus.antonius.felix@gmail.com>
2026-01-11 19:26:35 +0300

    Update autoprint.sh

--- autoprint.sh
+++ autoprint.sh
@@ -1,15 +1,16 @@
 sudo bash -c 'cat <<"EOF" > /home/proxyuser/autoprint.sh
 #!/bin/bash
-#
+
 echo "[$(date "+%Y-%m-%d %H:%M:%S")] Запуск скрипта" >> /tmp/autoprint.log
-#
+
 # Проверяем, не выполнялась ли уже печать сегодня
 if [[ -f /tmp/autoprint_$(date +"%Y.%m.%d") ]]; then
     echo "[$(date "+%Y-%m-%d %H:%M:%S")] Печать уже выполнена сегодня" >> /tmp/autoprint.log
     exit 0
 fi
-#
+
 R_FILE=$(ls -t /var/opt/sst-iiko/slip/*R* 2>/dev/null | head -1)
+
 # Проверяем наличие ошибки в логах
 if tail -n10000 $(ls -1td /var/log/sst-iiko/* 2>/dev/null) | grep "Failed to start operation" | grep $(date "+%Y.%m.%d")
 then
@@ -25,19 +26,20 @@ then
         echo "Ошибка: ни один принтер не доступен" >> /tmp/autoprint.log
         exit 1
     fi
-lp -d ${PRINTER} "${R_FILE}"
-echo "Печать успешно отправлена" >> /tmp/autoprint.log
-touch /tmp/autoprint_$(date +"%Y.%m.%d") # СОЗДАЕМ ФЛАГОВЫЙ ФАЙЛ!
+    
+    lp -d ${PRINTER} "${R_FILE}"
+    echo "Печать успешно отправлена" >> /tmp/autoprint.log
+    touch /tmp/autoprint_$(date +"%Y.%m.%d") # СОЗДАЕМ ФЛАГОВЫЙ ФАЙЛ!
 else
     #echo "Ошибка Failed to start operation не найдена в логах" >> /tmp/autoprint.log
     exit 1
 fi
-#
+
 echo "[$(date "+%Y-%m-%d %H:%M:%S")] Скрипт завершен" >> /tmp/autoprint.log
-EOF
-#
+EOF'
+
 chmod +x /home/proxyuser/autoprint.sh
-chown proxyuser:proxyuser /home/proxyuser/autoprint.sh'
-#
-echo '* 21-23 * * * proxyuser bash -c '\''[ ! -f "/tmp/autoprint_$(date +\%Y.\%m.\%d)" ] && [ -f /home/proxyuser/autoprint.sh ] && /home/proxyuser/autoprint.sh'\' | sudo tee /etc/cron.d/print-job
+chown proxyuser:proxyuser /home/proxyuser/autoprint.sh
+
+echo '* 21-23 * * * proxyuser bash -c '\''[ ! -f "/tmp/autoprint_$(date +\%Y.\%m.\%d)" ] && [ -f /home/proxyuser/autoprint.sh ] && /home/proxyuser/autoprint.sh'\'' | sudo tee /etc/cron.d/print-job
 sudo systemctl restart cron
